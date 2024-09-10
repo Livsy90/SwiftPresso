61670f5b1f071ca78a5620106aedd3d29168d05e
Livsy90 <artmir@live.ru>
2024-09-10 17:38:09 +0400

    Default PostListView

--- Sources/SwiftPresso/SwiftPresso+View.swift
+++ Sources/SwiftPresso/SwiftPresso+View.swift
@@ -5,12 +5,12 @@ extension SwiftPresso {
     /// View factory methods.
     public enum View {
         
-        public static func postListView<Placeholder: SwiftUI.View>(
+        public static func postListWithCustomPlaceholder<Placeholder: SwiftUI.View>(
             _ configuration: SwiftPressoConfiguration,
             placeholder: @escaping () -> Placeholder
         ) -> some SwiftUI.View {
-            Configuration.configure(with: configuration)
             
+            Configuration.configure(with: configuration)
             return PostListView(
                 backgroundColor: Configuration.backgroundColor,
                 interfaceColor: Configuration.interfaceColor,
@@ -29,7 +29,30 @@ extension SwiftPresso {
             )
         }
         
-        public static func shimmerPlaceholderView(backgroundColor: Color = Configuration.backgroundColor) -> some SwiftUI.View {
+        public static func postListWithDefaultPlaceholder(_ configuration: SwiftPressoConfiguration) -> some SwiftUI.View {
+            Configuration.configure(with: configuration)
+            
+            return PostListView(
+                backgroundColor: Configuration.backgroundColor,
+                interfaceColor: Configuration.interfaceColor,
+                textColor: Configuration.textColor,
+                menuBackgroundColor: Configuration.menuBackgroundColor,
+                menuTextColor: Configuration.menuTextColor,
+                homeIcon: Configuration.homeIcon,
+                isShowPageMenu: Configuration.isShowPageMenu,
+                isShowTagMenu: Configuration.isShowTagMenu,
+                isShowCategoryMenu: Configuration.isShowCategoryMenu,
+                pageMenuTitle: Configuration.pageMenuTitle,
+                tagMenuTitle: Configuration.tagMenuTitle,
+                categoryMenuTitle: Configuration.categoryMenuTitle,
+                postPerPage: Configuration.postsPerPage,
+                loadingPlaceholder: {
+                    shimmerPlaceholderView()
+                }
+            )
+        }
+        
+        public static func shimmerPlaceholder(backgroundColor: Color = Configuration.backgroundColor) -> some SwiftUI.View {
             SPShimmerPlacehodler(backgroundColor: backgroundColor)
         }
         
