c35e7deb71762a46a7fc6970ac016baef458de2d
Livsy90 <artmir@live.ru>
2024-12-22 20:32:35 +0400

    DeleteUserResponse

--- Sources/SwiftPresso/Model/User/DeleteUserResponse.swift
+++ Sources/SwiftPresso/Model/User/DeleteUserResponse.swift
@@ -1,3 +1,7 @@
 public struct DeleteUserResponse: Codable, Sendable {
     public let isDeleted: Bool
+    
+    private enum CodingKeys: String, CodingKey {
+        case isDeleted = "deleted"
+    }
 }
