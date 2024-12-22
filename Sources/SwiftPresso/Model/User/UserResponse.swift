ee20869497d9fc34cd7d2dd2a3a60236c5b668d4
Livsy90 <artmir@live.ru>
2024-12-22 19:59:55 +0400

    UserResponse

--- Sources/SwiftPresso/Model/User/UserResponse.swift
+++ Sources/SwiftPresso/Model/User/UserResponse.swift
@@ -4,8 +4,13 @@ import Foundation
 
 public struct UserResponse: Codable, Sendable {
     public let id: Int
-    public let username, name, firstName, lastName: String
-    public let email, url, description: String
+    public let username: String
+    public let name: String?
+    public let firstName: String?
+    public let lastName: String?
+    public let email: String?
+    public let url: String?
+    public let description: String?
 
     enum CodingKeys: String, CodingKey {
         case id, username, name
