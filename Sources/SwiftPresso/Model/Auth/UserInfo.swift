7c9e075b0e691c3fd6971e4a830e2dda3bb6cb53
Livsy90 <artmir@live.ru>
2024-12-23 19:13:06 +0400

    UserInfo

--- Sources/SwiftPresso/Model/Auth/UserInfo.swift
+++ Sources/SwiftPresso/Model/Auth/UserInfo.swift
@@ -1,9 +1,11 @@
 public struct UserInfo: Codable, Sendable {
     public let id: Int
     public let name: String
+    public let description: String
     
     private enum CodingKeys: String, CodingKey {
         case id
         case name
+        case description
     }
 }
